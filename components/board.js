cbcfde9b03519cbb2c8e83c2f58ed64270f64faf
breezy <apelibrian@gmail.com>
2021-10-10 11:39:41 +0300

    succesfully decrae a winner

--- components/board.js
+++ components/board.js
@@ -8,20 +8,46 @@ function Square(props) {
         </button>
     )
 }
-
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
 class Board extends Component {
     constructor(props) {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
-        xIsNext: true,
+            xIsNext: true,
         }
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = this.state.xIsNext ? 'X':'O';
-        this.setState({ squares: squares, xIsNext: !this.state.xIsNext,});
+
+        if(calculateWinner(squares) || squares[i]) {
+            return;
+        }
+
+        squares[i] = this.state.xIsNext ? "X": "O";
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
     renderSquare(i) {
         return <Square value={this.state.squares[i]}
@@ -29,8 +55,13 @@ class Board extends Component {
     }
 
     render() {
-        const status = 'Next player: '+(this.state.xIsNext ? 'X' : 'O');
-
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if(winner){
+            status = 'Winner: '+ winner;
+        }else{
+            status = 'Next player: ' + (this.state.xIsNext ? 'X': "O")
+        }
         return (
             <div>
                 <div className="status">{status}</div>
